1960d09e20c681c4f4e0f493f3d80aff453a8596
dimtep <teplov.dmitriy.anatolevich@gmail.com>
2021-05-27 11:54:59 +0300

    movies
    card

--- app.js
+++ app.js
@@ -11,45 +11,60 @@ const main = document.querySelector('.main');
 button.onclick = function(event) {
   event.preventDefault();
   const value = input.value;
-  document.querySelector('.main').innerHTML = "";
+  main.innerHTML = "";
   const newUrl = API_URL + '&query=' + value;
-  fetch(newUrl)
+  getMovies(newUrl)
+}
+function getMovies(url) {
+  fetch(url)
     .then((response => response.json()))
-    .then(function(data){
-      data.results.forEach(element => {
-        
-        const mainCard = document.createElement('div');
-        mainCard.classList.add('main-card');
-        const image = document.createElement('img');
-        const title = document.createElement('h2');
-        title.classList.add('main-title');
-        const release = document.createElement('p');
-        release.classList.add('main-release');
-        const timeMoonSec = Date.parse(`${element.release_date}`);
-        fetch(`http://api.farmsense.net/v1/moonphases/?d=${timeMoonSec}`)
-        .then((response => response.json()))
-        .then((results) => {
-          results.forEach(elem =>{
-            
-            const moonPhase = document.createElement('p');
-            moonPhase.innerHTML = 'Phase Moon: ' + `${elem.Phase}`;
-            mainCard.appendChild(moonPhase);
-          })
-
-        })
-        
-        title.innerHTML = `${element.title}`;
-        release.innerHTML = 'Release: ' + `${element.release_date}`;
-        image.src = IMGPATH + element.poster_path;
-        mainCard.appendChild(image);
-        mainCard.appendChild(title);
-        mainCard.appendChild(release);
-        main.appendChild(mainCard); 
-      }) 
-
+    .then((data) =>{
+      showMovies(data)
     })
     .catch((err)=>{
       alert(err + " Something went wrong..!");
     })
 }
+    
+function showMovies(data) {
+  data.results.forEach(element => {
+      
+    const mainCard = document.createElement('div');
+    mainCard.classList.add('main-card');
+    const image = document.createElement('img');
+    const title = document.createElement('h2');
+    title.classList.add('main-title');
+    const release = document.createElement('p');
+    release.classList.add('main-release');
+    const timeMoonSec = Date.parse(`${element.release_date}`);
+    getMoonPhase(timeMoonSec,mainCard);
+
+    title.innerHTML = `${element.title}`;
+    release.innerHTML = 'Release: ' + `${element.release_date}`;
+    image.src = IMGPATH + element.poster_path;
+    mainCard.appendChild(image);
+    mainCard.appendChild(title);
+    mainCard.appendChild(release);
+    main.appendChild(mainCard); 
+  });
+} 
+function getMoonPhase(timeMoonSec,mainCard) {
+  fetch(`http://api.farmsense.net/v1/moonphases/?d=${timeMoonSec}`)
+  .then((response => response.json()))
+  .then((data) =>{
+     showMoonPhase(data,mainCard)
+    })
+    .catch((err)=>{
+      alert(err + " Something went wrong..!");
+    })
+  }
+function showMoonPhase (data,mainCard) {
+  data.forEach(element =>{
+    
+    const moonPhase = document.createElement('p');
+    moonPhase.innerHTML = 'Phase Moon: ' + `${element.Phase}`;
+    mainCard.appendChild(moonPhase);
+  })
+}
+
 
